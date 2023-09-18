c4a7dc078ddb1c7938236518ec03f51d6d0504b4
Computer PsiPhi <blotted.209@gmail.com>
2023-09-18 13:19:26 -0700

    Homework folder

---
+++ Homework/Delete_NOW/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: blott
+ *
+ * Created on September 18, 2023, 1:14 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"HELLO";
+    return 0;
+}
+
