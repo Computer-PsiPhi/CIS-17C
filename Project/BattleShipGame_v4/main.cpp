fc3ffa4e6aa483f9a2a77ebe00175341c1e03ad6
Computer PsiPhi <blotted.209@gmail.com>
2023-10-29 21:14:10 -0700

    Create v4

---
+++ Project/BattleShipGame_v4/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: blott
+ *
+ * Created on October 29, 2023, 9:13 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
