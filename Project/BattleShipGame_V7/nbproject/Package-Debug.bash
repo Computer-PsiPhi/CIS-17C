41c95c336a6db493789a91e9401fbb52a87a1d8e
Computer PsiPhi <blotted.209@gmail.com>
2023-11-15 03:02:41 -0800

    Latest push

--- Project/BattleShipGame_V7/nbproject/Package-Debug.bash
+++ Project/BattleShipGame_V7/nbproject/Package-Debug.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/battleshipgame_v6
-OUTPUT_BASENAME=battleshipgame_v6
-PACKAGE_TOP_DIR=battleshipgamev6/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/battleshipgame_v7
+OUTPUT_BASENAME=battleshipgame_v7
+PACKAGE_TOP_DIR=battleshipgamev7/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/battleshipgamev6/bin"
+makeDirectory "${NBTMPDIR}/battleshipgamev7/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/battleshipgamev6.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/battleshipgamev7.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/battleshipgamev6.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/battleshipgamev7.tar *
 checkReturnCode
 
 # Cleanup
--- Project/BattleShipGame_v7/nbproject/Makefile-impl.mk
+++ Project/BattleShipGame_v7/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=BattleShipGame_v6
+PROJECTNAME=BattleShipGame_v7
 
 # Active Configuration
 DEFAULTCONF=Debug
