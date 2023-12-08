58fd8d5c58da3138a205d904fc507d2c910da044
Computer PsiPhi <blotted.209@gmail.com>
2023-12-08 15:21:15 -0800

    Latest push

--- Projects/Version Control For Project-All Previous Versions/BattleShipGame_v1/nbproject/Makefile-variables.mk
+++ Projects/Version Control For Project-All Previous Versions/BattleShipGame_v1/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=battleshipgame
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/battleshipgame
+CND_ARTIFACT_NAME_Debug=battleshipgame_v1
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/battleshipgame_v1
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=battleshipgame.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/battleshipgame.tar
+CND_PACKAGE_NAME_Debug=battleshipgamev1.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/battleshipgamev1.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=battleshipgame
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/battleshipgame
+CND_ARTIFACT_NAME_Release=battleshipgame_v1
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/battleshipgame_v1
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=battleshipgame.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/battleshipgame.tar
+CND_PACKAGE_NAME_Release=battleshipgamev1.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/battleshipgamev1.tar
 #
 # include compiler specific variables
 #
